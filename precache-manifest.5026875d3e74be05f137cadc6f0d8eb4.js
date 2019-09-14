ddb97a0e5c362524c378b8e1858a1b44c85426ce
salvadorsalgado1 <salvadorsalgado5694@gmail.com>
2019-09-13 20:39:20 -0700

    Updates

---
+++ precache-manifest.5026875d3e74be05f137cadc6f0d8eb4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "109ff06f7cee16d73a8b9fb0e4a661eb",
+    "url": "/champions/index.html"
+  },
+  {
+    "revision": "fa4bdf661b85c95d8bc6",
+    "url": "/champions/static/css/main.9b6ecf50.chunk.css"
+  },
+  {
+    "revision": "0f3147e4e277284a323f",
+    "url": "/champions/static/js/2.dc957d51.chunk.js"
+  },
+  {
+    "revision": "fa4bdf661b85c95d8bc6",
+    "url": "/champions/static/js/main.43ece7f7.chunk.js"
+  },
+  {
+    "revision": "a68dba9f8ad032a042aa",
+    "url": "/champions/static/js/runtime~main.b1d7c7c7.js"
+  },
+  {
+    "revision": "d5049f2a7aee699c8536b98cd5d61868",
+    "url": "/champions/static/media/ball.d5049f2a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/champions/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fe9b049a457ed442802c3977be6f139c",
+    "url": "/champions/static/media/net.fe9b049a.jpg"
+  },
+  {
+    "revision": "c5958a0e968794726f7be982ac58bb59",
+    "url": "/champions/static/media/srim.c5958a0e.jpg"
+  }
+]);
